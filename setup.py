4a69b6293061bf7c572453124077a3f2baacc321
dhanushk-offl <dhanushkandhan75@gmail.com>
2024-12-26 17:04:29 +0530

    Linkage Update ✨

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     install_requires=["psutil", "GPUtil", "rich"],
     entry_points={
         "console_scripts": [
-            "sysoptai=sysoptai.main:main"
+            "systoptia=systoptia.main:main"
         ]
     },
     author='Dhanush Kandhan',
