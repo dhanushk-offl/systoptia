0c10a1c64a2e9d680650380caecfcd88fef89dd3
dhanushk-offl <dhanushkandhan75@gmail.com>
2024-12-26 19:04:35 +0530

    Once Again Ya ✨

--- setup.py
+++ setup.py
@@ -1,24 +1,22 @@
+# setup.py
 from setuptools import setup, find_packages
 
 setup(
     name="systoptia",
-    version="1.0.0",
-    packages=find_packages(),
-    install_requires=["psutil", "GPUtil", "rich"],
-    entry_points={
-        "console_scripts": [
-            "systoptia=systoptia.main:main"
-        ]
-    },
-    author='Dhanush Kandhan',
-    description='A CLI tool for Monitoring, Optimizing, and Refactoring Embedded and Computer Systems',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/dhanushk-offl/systoptia',
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+    version="0.1.0",
+    packages=find_packages(include=['*']),
+    install_requires=[
+        'click>=8.0.0',
+        'psutil>=5.8.0',
+        'GPUtil>=1.4.0',
+        'rich>=10.0.0'
     ],
-    python_requires='>=3.8',
+    entry_points={
+        'console_scripts': [
+            'systoptia=main:main'  # Point directly to your main.py's main function
+        ],
+    },
+    author="Dhanush K",
+    description="A comprehensive system optimization and monitoring tool",
+    python_requires=">=3.6",
 )
